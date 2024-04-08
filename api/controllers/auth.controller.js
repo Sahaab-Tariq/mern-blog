582e9e6e7d9f2c6a8abc34a1d5c9eab1268447c9
Sahaab-Tariq <sahabtariq888@gmail.com>
2024-04-08 21:00:18 +0500

    add functionality to the sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,16 +2,14 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 
-export const signup = async(req,res,next)=>{
-    const {username, email, password} = req.body;
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
 
-    if(!username || !email || !password || username=== '' || email=== '' || password=== ''){
-        return res.status(400).json({message: 'All fields are required'})
-    }{
-        next(errorHandler(400, 'All fields are required'));
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return res.status(400).json({ message: 'All fields are required' });
     }
 
-    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const hashedPassword = bcryptjs.hashSync(password, 10);
 
     const newUser = new User({
         username,
@@ -21,10 +19,39 @@ export const signup = async(req,res,next)=>{
 
     try {
         await newUser.save();
-        res.json({message: 'Signup successful'});
-        
+        res.json({ message: 'Signup successful' });
     } catch (error) {
         next(error);
     }
+};
 
-}
+// import User from '../models/user.model.js';
+// import bcryptjs from 'bcryptjs';
+// import { errorHandler } from '../utils/error.js';
+
+// export const signup = async(req,res,next)=>{
+//     const {username, email, password} = req.body;
+
+//     if(!username || !email || !password || username=== '' || email=== '' || password=== ''){
+//         return res.status(400).json({message: 'All fields are required'})
+//     }{
+//         next(errorHandler(400, 'All fields are required'));
+//     }
+
+//     const hashedPassword = bcryptjs.hashSync(password, 10)
+
+//     const newUser = new User({
+//         username,
+//         email,
+//         password: hashedPassword,
+//     });
+
+//     try {
+//         await newUser.save();
+//         res.json({message: 'Signup successful'});
+        
+//     } catch (error) {
+//         next(error);
+//     }
+
+// }
